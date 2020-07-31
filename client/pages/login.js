e7ec1b93372773e6fee9b993382609b790630c2b
Umar Yousafzai <39500143+uyousafzai54@users.noreply.github.com>
2020-07-31 15:33:25 -0400

    add login page to pages

---
+++ client/pages/login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View, Image } from "react-native";
+import {Formik} from 'formik'; 
+import styles from "../styles/homepage";
+import Button from "../components/button";
+
+export default function login() {
+	return (
+		<View style={styles.container}>
+			
+		</View>
+	);
+}
