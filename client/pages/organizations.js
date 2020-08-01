04f020a945afc47e52ca5dba18b21bedff1caf1c
sunny-zuo <me@sunnyzuo.com>
2020-07-31 20:31:25 -0600

    feat: render list of organizations

--- client/pages/organizations.js
+++ client/pages/organizations.js
@@ -13,18 +13,25 @@ import styles from "../styles/welcomepage";
 
 export default function organizations(props) {
 	const [data, setData] = useState();
-	const url = "https://localhost:8000/organizations/getList";
+	const url = "http://159.203.16.113:3000/organizations/getList";
 	async function retrieveData() {
+		let jwt = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkFkYW0gU21pdGgiLCJlbWFpbCI6ImNhcHRpYWxpc21AZ21haWwuY29tIiwiaWF0IjoxNTk2MjI2NTM4fQ.b9LId0Y192Ii5uqjPCsJAkBxkf2RxlTWiLNNXgTntto';
 		try {
-			let response = await fetch(url);
+			let response = await fetch(url, {
+				method: 'GET',
+				headers: {
+					'Authorization': `Bearer ${jwt}`
+				}
+			});
 			let responseJson = await response.json();
-			console.log("Hi");
-			console.log(responseJson); //data
+			setData(responseJson);
 		} catch (error) {
 			console.log(error);
 		}
 	}
-	retrieveData();
+	if (!data) {
+		retrieveData();
+	}
 
 	// const organizations = [
 	// 	{
@@ -46,25 +53,27 @@ export default function organizations(props) {
 		console.log(isVisible);
 	};
 
+	const renderItem = ({ item }) => (
+		<TouchableOpacity
+			onPress={() =>
+				props.navigation.navigate("OrganizationDetails", item)
+			}
+		>
+			<Card>
+				<Text>{item.name}</Text>
+				<Text>{item.description}</Text>
+			</Card>
+		</TouchableOpacity>
+	);
+
 	return (
 		<View style={styles.container}>
-			<Text style={{marginTop: 100}}> Organizations </Text>{/* 
+			<Text style={{marginTop: 100}}> Organizations </Text>
 			<FlatList
-				data={organizations}
-				renderItem={({ data }) => (
-					<TouchableOpacity
-						onPress={() =>
-							props.navigation.navigate("OrganizationDetails", item)
-						}
-					>
-						<Card>
-							<Text>{item.name}</Text>
-							<Text>{item.description}</Text>
-							<Text>{item.representatives}</Text>
-						</Card>
-					</TouchableOpacity>
-				)} 
-			/>*/}
+				data={data}
+				renderItem={renderItem}
+				keyExtractor={(item) => item.id}
+			/>
 			<NewOrgBtn text="+" onPress={onPressPlus}></NewOrgBtn>
 			<AddOrgMenu isVisible={isVisible}></AddOrgMenu> 
 		</View>
