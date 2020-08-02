0fed6eab9cca757b52449f1b493e43a063c96079
sunny-zuo <me@sunnyzuo.com>
2020-08-01 18:29:53 -0600

    fix: render welcome screen regardless & navigate if token exists

--- client/pages/welcome.js
+++ client/pages/welcome.js
@@ -5,50 +5,44 @@ import Button from "../components/button";
 import AsyncStorage from "@react-native-community/async-storage";
 
 export default function welcome(props) {
-	const getToken = async () => {
-		try {
-			//item is given back as string
-			return AsyncStorage.getItem("Token");
-		} catch (error) {
-			console.log("Something went wrong", error);
-		}
-	};
-
-	getToken().then((response) => {
+	AsyncStorage.getItem("Token").then((response) => {
 		if (response) {
+			console.log(`get token response: ${response}`)
 			props.navigation.navigate("Organizations");
-		} else {
-			return (
-				<View style={styles.container}>
-					<Image
-						source={require("../assets/background.jpg")}
-						style={styles.backgroundImage}
-					/>
-					<View style={styles.textContainer}>
-						<Text style={styles.text}>WELCOME TO</Text>
-						<Text style={styles.textTitle}>BetterVOTE</Text>
-						<Text style={styles.text}>
-							Organizing communities, one vote at a time
-						</Text>
-					</View>
+		}
+	}).catch((err) => {
+		console.log(`Error when checking if token exists: ${err}`);
+	});
 
-					<View style={styles.buttonContainer}>
-						<Button
-							text="Login"
-							onPress={() => props.navigation.navigate("Login")}
-						/>
-						<Button
-							text="Register"
-							onPress={() => props.navigation.navigate("Registration")}
-						/>
-						{/* <Button
+	return (
+		<View style={styles.container}>
+			<Image
+				source={require("../assets/background.jpg")}
+				style={styles.backgroundImage}
+			/>
+			<View style={styles.textContainer}>
+				<Text style={styles.text}>WELCOME TO</Text>
+				<Text style={styles.textTitle}>BetterVOTE</Text>
+				<Text style={styles.text}>
+					Organizing communities, one vote at a time
+						</Text>
+			</View>
+
+			<View style={styles.buttonContainer}>
+				<Button
+					text="Login"
+					onPress={() => props.navigation.navigate("Login")}
+				/>
+				<Button
+					text="Register"
+					onPress={() => props.navigation.navigate("Registration")}
+				/>
+				{/* <Button
 							text="Organizations"
 							onPress={() => props.navigation.navigate("Organizations")}
 						/> */}
-						{/*<Button text = "Terms of Service" onPress = {() => onPress("TOS")} /> */}
-					</View>
-				</View>
-			);
-		}
-	});
+				{/*<Button text = "Terms of Service" onPress = {() => onPress("TOS")} /> */}
+			</View>
+		</View>
+	);
 }
