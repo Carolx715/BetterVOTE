e4520e12f9c3dcf58a99de6fe0d84f8bdefffb82
Rahul Aggarwal git config --global user.email rahulaggarwal1016git config --global user.name Your <rahulaggarwal1016@gmail.com>
2020-08-02 15:16:37 -0400

    Merge branch 'master

--- client/pages/organizations.js
+++ client/pages/organizations.js
@@ -1,3 +1,4 @@
+//organizations page
 import React, { useState, useEffect } from "react";
 import {
 	Text,
@@ -6,7 +7,7 @@ import {
 	TouchableOpacity,
 	TouchableWithoutFeedback,
 	Image,
-	Keyboard
+	Keyboard,
 } from "react-native";
 import Card from "../components/card";
 import NewOrgBtn from "../components/addNewOrgBtn";
