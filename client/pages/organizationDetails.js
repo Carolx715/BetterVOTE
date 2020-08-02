c30fdaadc366f968febf2d962a08fb219d1bb678
Rahul Aggarwal git config --global user.email rahulaggarwal1016git config --global user.name Your <rahulaggarwal1016@gmail.com>
2020-08-02 00:00:20 -0400

    changes

--- client/pages/organizationDetails.js
+++ client/pages/organizationDetails.js
@@ -1,51 +1,68 @@
-import React, { useState } from "react";
-import { Text, View, FlatList, TouchableOpacity, ScrollView, StyleSheet	} from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+	Text,
+	View,
+	FlatList,
+	TouchableOpacity,
+	ScrollView,
+	StyleSheet,
+} from "react-native";
 import Card from "../components/card";
 import styles from "../styles/welcomepage";
 
 export default function OrganizationDetails(props) {
+	useEffect(() => {
+		retrieveDataDetails();
+	});
 
-	const renderItem = ({ item }) => (
-		<TouchableOpacity>
-			<Card>
-				<Text>{props.navigation.getParam("description")}</Text>
-			</Card>
-			<Card>
-				<Text> {props.navigation.getParam("representatives")}</Text>
-			</Card>
-		</TouchableOpacity>
-	);
+	const [data, setData] = useState();
+	id = props.navigation.getParam("_id");
+	const url = `http://159.203.16.113:3000/organizations/getOrg?id=${id}`;
+
+	async function retrieveDataDetails() {
+		let jwt = await AsyncStorage.getItem("Token").catch((err) => {
+			console.log(err);
+		});
+		try {
+			let response = await fetch(url, {
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${jwt}`,
+				},
+			});
+			console.log(response.json());
+			setData(response.json());
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
 	return (
 		<ScrollView>
-			<View style = {styles.flatlistContainer}>
-			<Text style={styles.textTitle2}>{props.navigation.getParam("name")}</Text>
-			<TouchableOpacity>
+			<View style={styles.flatlistContainer}>
+				<Text style={styles.textTitle2}>{data.name}</Text>
 				<Card>
 					<Text>DESCRIPTION</Text>
-					<Text>{props.navigation.getParam("description")}</Text>
+					<Text>{data.description}</Text>
 				</Card>
-			</TouchableOpacity>
-			<TouchableOpacity>
 				<Card>
 					<Text>YOUR REPRESENTATIVE</Text>
 					<Text>John Doe</Text>
-					<Text> {props.navigation.getParam("representatives")}</Text>
+					<Text>{data.representatives[0].username}</Text>
 				</Card>
-			</TouchableOpacity>
-			
-			<Text style = {styles.textTitle2}>On the Ballot</Text>
-			<TouchableOpacity>
-				<Card>
-					<Text>Sample Vote 1</Text>
-				</Card>
+
+				<Text style={styles.textTitle2}>On the Ballot</Text>
+				<TouchableOpacity>
+					<Card>
+						<Text>Sample Vote 1</Text>
+					</Card>
 				</TouchableOpacity>
 				<TouchableOpacity>
-				<Card>
-					<Text>Sample Vote 2.</Text> 
-				</Card>
-				</TouchableOpacity>		
-			</View>	
+					<Card>
+						<Text>Sample Vote 2.</Text>
+					</Card>
+				</TouchableOpacity>
+			</View>
 		</ScrollView>
 	);
 }
@@ -67,12 +84,12 @@ const styles2 = StyleSheet.create({
 		flex: 2,
 	},
 	scrollView: {
-		backgroundColor: 'pink',
+		backgroundColor: "pink",
 		marginHorizontal: 20,
-	  },	
+	},
 	cardContent: {
 		marginHorizontal: 18,
 		marginVertical: 20,
-		width: "100%"
+		width: "100%",
 	},
 });
